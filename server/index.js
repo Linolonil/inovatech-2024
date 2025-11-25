7c9f8379f85b2006ab64edaf529f32faf1861de3
lino jorge <linox999@gmail.com>
2025-11-24 23:48:53 -0400

    corrigindo importacao

--- server/index.js
+++ server/index.js
@@ -3,18 +3,15 @@ import dotenv from "dotenv";
 import http from "http";
 import path from "path";
 
-import apiRoutes from "./routes/apiRoutes.js";
-import usuarioRoutes from "./routes/usuarioRoutes.js";
-import sensorRoutes from "./routes/sensorRoutes.js";
-import loginRoutes from "./routes/loginRoutes.js";
-import validadeTokenUser from "./routes/validateToken.js";
-import {
-  getDataSensor03,
-  initializeWebSocket,
-} from "./services/websocketService.js";
-import connectDB from "./config/db.js";
-import express from "express";
 import cors from "cors";
+import express from "express";
+import connectDB from "./config/db.js";
+import apiRoutes from "./routes/apiRoutes.js";
+import loginRoutes from "./routes/loginRoutes.js";
+import sensorRoutes from "./routes/sensorRoutes.js";
+import usuarioRoutes from "./routes/usuarioRoutes.js";
+import validadeTokenUser from "./routes/validateToken.js";
+import { initializeWebSocket } from "./services/websocketService.js";
 
 const app = express();
 
@@ -40,27 +37,6 @@ app.use("/api/sensores", sensorRoutes);
 app.use("/api/login", loginRoutes);
 app.use("/api/v1/auth/validate-token", validadeTokenUser);
 
-//  gambiarra inicio
-app.get("/api/sensor", async (req, res) => {
-  try {
-    if (true) {
-      const responseData = {
-        status: "success",
-        sensorId: getDataSensor03().sensorId,
-        data: getDataSensor03(),
-        message: `Dados do sensor 003 enviados com sucesso.`,
-      };
-      return res.status(200).json(responseData);
-    }
-  } catch (error) {
-    console.error("Erro ao obter dados do sensor 003:", error);
-    res.status(500).json({
-      status: "error",
-      message: "Erro ao obter dados do sensor 003.",
-      error: error.message, // Inclui uma mensagem de erro para depuração
-    });
-  }
-});
 //  gambiarra fim
 
 // Inicializa o WebSocket
